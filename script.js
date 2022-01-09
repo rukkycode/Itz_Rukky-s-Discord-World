1dd268d01b9d0d5f23ebf3cadecf048b0d213034
Itz_Rukky <74117698+rukkycode@users.noreply.github.com>
2022-01-09 16:04:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// No code in this file yet...
+// Basic Console UI
+
+function run(message) {
+  message = "Hello";
+
+  if(run=true) {
+  console.log(message);
+}
+
+}
+
+run()
+
+// Not much for now Sorry :(
